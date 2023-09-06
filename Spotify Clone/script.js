3d25e35081be3d11d1c7bdadb15062cf38d350fa
ayush7898 <141718896+ayush7898@users.noreply.github.com>
2023-09-06 14:06:48 +0530

    Add files via upload

---
+++ Spotify Clone/script.js
@@ -0,0 +1,93 @@
+const music = new Audio('./audio/1.mp3');
+const songs = [
+    {
+        id:'1',
+        songName:`On My Way <br>
+        <div class="subtitle">Alan Walker</div>`,
+        poster:"img/1.jpg"
+    },
+    {
+        id:'2',
+        songName:`Alan Walker-Fade <br>
+        <div class="subtitle">Alan Walker</div>`,
+        poster:"img/2.jpg"
+    },
+    {
+        id:'3',
+        songName:`Cartoon - On & On <br>
+        <div class="subtitle">Daniel Levi</div>`,
+        poster:"img/3.jpg"
+    },
+    {
+        id:'4',
+        songName:`Warriyo - Mortals <br>
+        <div class="subtitle">Mortals</div>`,
+        poster:"img/4.jpg"
+    },
+    {
+        id:'5',
+        songName:`Ertugrul Gazi <br>
+        <div class="subtitle">Ertugrul</div>`,
+        poster:"img/5.jpg"
+    },
+    {
+        id:'6',
+        songName:`Electornic Music <br>
+        <div class="subtitle">Electro</div>`,
+        poster:"img/6.jpg"
+    },
+    {
+        id:'7',
+        songName:`Agar Tum Saath Ho <br>
+        <div class="subtitle">Alka Yagnik</div>`,
+        poster:"img/7.jpg"
+    },
+    {
+        id:'8',
+        songName:`Suna Hai <br>
+        <div class="subtitle">Neha Kakkar</div>`,
+        poster:"img/8.jpg"
+    },
+    {
+        id:'9',
+        songName:`Dilbar <br>
+        <div class="subtitle">Satyameva Jayate</div>`,
+        poster:"img/9.jpg"
+    },
+    {
+        id:'10',
+        songName:`Duniya <br>
+        <div class="subtitle">Luka Chuppi</div>`,
+        poster:"img/10.jpg"
+    },
+    {
+        id:'11',
+        songName:`Lagdi Lahore <br>
+        <div class="subtitle">Street Dancer</div>`,
+        poster:"img/11.jpg"
+    },
+    {
+        id:'12',
+        songName:`Putt Jatt Da <br>
+        <div class="subtitle">Putt Jatt Da</div>`,
+        poster:"img/12.jpg"
+    },
+    {
+        id:'13',
+        songName:`Baarishein <br>
+        <div class="subtitle">Atif Aslam</div>`,
+        poster:"img/13.jpg"
+    },
+    {
+        id:'14',
+        songName:`Vaaste<br>
+        <div class="subtitle">Dhvani Bhanushali</div>`,
+        poster:"img/14.jpg"
+    },
+    {
+        id:'15',
+        songName:`Lut Gaye <br>
+        <div class="subtitle">Jubin Nautiyal</div>`,
+        poster:"img/15.jpg"
+    }
+]
